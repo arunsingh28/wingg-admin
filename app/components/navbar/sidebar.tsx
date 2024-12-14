e5fd0abcd88a7c3bb97bd743d70147f8a2e46776
Arun Singh <arunsingh28aug.as@gmail.com>
2024-12-14 23:40:27 +0530

    Update sidebar component to use Link for navigation in menu items

--- app/components/navbar/sidebar.tsx
+++ app/components/navbar/sidebar.tsx
@@ -2,24 +2,25 @@ import { Menu } from "antd";
 import { IoMdHome } from "react-icons/io";
 import { IoShieldHalfSharp } from "react-icons/io5";
 import { TbTemplate } from "react-icons/tb";
-
 import type { MenuProps } from "antd";
+import { Link } from "@remix-run/react";
+
 
 const items: MenuProps["items"] = [
   {
     key: "1",
     icon: <IoMdHome />,
-    label: "Dashboard",
+    label: <Link to={"/dash/home"}>Dashboard</Link>,
   },
   {
     key: "2",
     icon: <IoShieldHalfSharp />,
-    label: "Permissions",
+    label: <Link to={"/dash/permissions"}>Permissions</Link>,
   },
   {
     key: "3",
     icon: <TbTemplate />,
-    label: "Email templates",
+    label:<Link to={"/dash/email-templates"}>Email templates</Link>,
   },
 ];
 
